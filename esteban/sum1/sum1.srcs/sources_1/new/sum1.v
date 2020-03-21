d8a27b0167f1bb195ed2b83237f096e8879c207b
esteban11519 <eladinof@unal.edu.co>
2020-03-21 12:16:25 -0500

    Practica sumador de un bit

---
+++ esteban/sum1/sum1.srcs/sources_1/new/sum1.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/21/2020 12:14:02 PM
+// Design Name: 
+// Module Name: sum1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sum1bcc (A, B, Ci,Cout,S);
+
+  input  A;
+  input  B;
+  input  Ci;
+  output Cout;
+  output S;
+
+  reg [1:0] st;
+  assign S = st[0];
+  assign Cout = st[1];
+
+  always @ ( * ) begin
+  	st  = 	A+B+Ci;
+  end
+  
+endmodule
